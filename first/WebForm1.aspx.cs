71da06a7e85a5de177205b4e5ea961cdde47324e
LINLIANG <73630520+374139047@users.noreply.github.com>
2021-08-04 18:37:34 +0800

    exercise

---
+++ first/WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace first
+{
+	public class WebForm1 : System.Web.UI.Page
+	{
+		protected System.Web.UI.WebControls.Label Label1;
+		protected System.Web.UI.WebControls.Label Label2;
+		protected System.Web.UI.WebControls.Label Label3;
+		protected System.Web.UI.WebControls.TextBox TextBox1;
+		protected System.Web.UI.WebControls.TextBox TextBox2;
+		protected System.Web.UI.WebControls.TextBox TextBox3;
+		protected System.Web.UI.WebControls.Button Button1;
+		protected System.Web.UI.WebControls.Label Label4;
+	
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+		}
+
+		#region Web 窗体设计器生成的代码
+		override protected void OnInit(EventArgs e)
+		{
+			//
+			// CODEGEN: 该调用是 ASP.NET Web 窗体设计器所必需的。
+			//
+			InitializeComponent();
+			base.OnInit(e);
+		}
+		
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要使用代码编辑器修改
+		/// 此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{    
+			this.Button1.Click += new System.EventHandler(this.Button1_Click);
+			this.Load += new System.EventHandler(this.Page_Load);
+
+		}
+		#endregion
+
+		private void Button1_Click(object sender, System.EventArgs e)
+		{
+			double FanQian = double.Parse(TextBox1.Text);
+			double ZheKou  = double.Parse(TextBox2.Text);
+			double ZongJi  = FanQian*ZheKou ;
+			TextBox3.Text = Convert.ToString(ZongJi);
+
+		}
+	}
+}
